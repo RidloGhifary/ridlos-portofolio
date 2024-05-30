2fedc388a1573280d4f33418629f7c1eb49d134a
RidloGhifary <ridloghfry@gmail.com>
2024-05-30 13:27:33 +0700

    fix: fix image path

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,12 @@ const App = () => {
   ];
 
   return (
-    <main className="bg-AlmostBlack text-AlmostWhite overflow-hidden">
+    <main className="overflow-hidden bg-AlmostBlack text-AlmostWhite">
       <div className="mx-auto max-w-4xl px-4 lg:px-0">
         <section className="relative flex min-h-screen flex-col items-start justify-center pt-20 md:items-start md:justify-end md:py-2 md:pt-0">
           <h1
             data-aos="fade-down"
-            className="Kalnia selection:bg-AlmostWhite selection:text-AlmostBlack text-7xl font-bold md:text-9xl"
+            className="Kalnia text-7xl font-bold selection:bg-AlmostWhite selection:text-AlmostBlack md:text-9xl"
           >
             H`i
             <br />
@@ -38,7 +38,7 @@ const App = () => {
           <div className="mt-6 grid gap-8 md:grid-cols-2">
             <p
               data-aos="fade-down"
-              className="selection:bg-AlmostWhite selection:text-AlmostBlack leading-7"
+              className="leading-7 selection:bg-AlmostWhite selection:text-AlmostBlack"
             >
               This portfolio is my testament, a showcase of my blossoming skills
               and the fruits of my tireless learning. It&apos;s an invitation to
@@ -53,7 +53,7 @@ const App = () => {
             >
               {skillsList?.map((data) => (
                 <span
-                  className="bg-SecondaryColor select-none rounded-xl px-4 py-2 font-semibold text-white"
+                  className="select-none rounded-xl bg-SecondaryColor px-4 py-2 font-semibold text-white"
                   key={data}
                 >
                   #{data}
@@ -64,7 +64,7 @@ const App = () => {
 
           <ul
             data-aos="fade-left"
-            className="selection:bg-AlmostWhite selection:text-AlmostBlack absolute right-[50%] top-0 mt-3 flex translate-x-[50%] gap-14 lg:-right-28 lg:top-[30%] lg:mt-0 lg:translate-x-0 lg:flex-col"
+            className="absolute right-[50%] top-0 mt-3 flex translate-x-[50%] gap-14 selection:bg-AlmostWhite selection:text-AlmostBlack lg:-right-28 lg:top-[30%] lg:mt-0 lg:translate-x-0 lg:flex-col"
           >
             <li className="text-center hover:underline lg:rotate-90">
               <Link to={"https://medium.com/@ridloghfry"} target="_blank">
@@ -90,7 +90,7 @@ const App = () => {
         <section className="py-40">
           <h1
             data-aos="fade-down"
-            className="Kalnia selection:bg-AlmostWhite selection:text-AlmostBlack before:stroke-AlmostWhite text-center text-4xl font-bold uppercase md:text-7xl"
+            className="Kalnia text-center text-4xl font-bold uppercase selection:bg-AlmostWhite selection:text-AlmostBlack before:stroke-AlmostWhite md:text-7xl"
           >
             project
           </h1>
@@ -98,7 +98,7 @@ const App = () => {
             {projectList?.map((data, index: number) => (
               <div
                 data-aos="fade-up"
-                className="border-AlmostWhite/50 mt-16 grid items-center gap-4 rounded-xl border p-5 md:grid-cols-2"
+                className="mt-16 grid items-center gap-4 rounded-xl border border-AlmostWhite/50 p-5 md:grid-cols-2"
                 key={index}
               >
                 <div
@@ -106,29 +106,29 @@ const App = () => {
                   className={`border-AlmostWhite ${index % 2 === 0 ? "order-last" : null} cursor-pointer select-none rounded-lg`}
                 >
                   <img
-                    src={data.image}
+                    src={import.meta.env.BASE_URL + data.image}
                     alt={data.title}
                     className="h-[300px] w-full rounded-lg object-cover object-top transition-all duration-[7s] ease-linear hover:object-bottom"
                   />
                 </div>
                 <div data-aos="fade-left" className="space-y-6 text-center">
-                  <h2 className="Kalnia text-SecondaryColor selection:bg-SecondaryColor selection:text-AlmostBlack text-5xl font-semibold">
+                  <h2 className="Kalnia text-5xl font-semibold text-SecondaryColor selection:bg-SecondaryColor selection:text-AlmostBlack">
                     {data.title}
                   </h2>
-                  <p className="selection:bg-AlmostWhite selection:text-AlmostBlack font-light">
+                  <p className="font-light selection:bg-AlmostWhite selection:text-AlmostBlack">
                     {data.desc}
                   </p>
                   <div className="flex select-none items-center justify-center gap-5">
                     <Link
                       to={data.linkDemo}
-                      className="hover:bg-AlmostWhite/20 bg-AlmostWhite/5 flex cursor-pointer items-center justify-center gap-2 rounded-xl px-4 py-2 transition"
+                      className="flex cursor-pointer items-center justify-center gap-2 rounded-xl bg-AlmostWhite/5 px-4 py-2 transition hover:bg-AlmostWhite/20"
                     >
                       <ExternalLink />
                       Demo
                     </Link>
                     <Link
                       to={data.linkCode}
-                      className="hover:bg-AlmostWhite/20 bg-AlmostWhite/5 flex cursor-pointer items-center justify-center gap-2 rounded-xl px-4 py-2 transition"
+                      className="flex cursor-pointer items-center justify-center gap-2 rounded-xl bg-AlmostWhite/5 px-4 py-2 transition hover:bg-AlmostWhite/20"
                     >
                       <Code />
                       Code
