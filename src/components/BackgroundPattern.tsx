1bf9cd4cf6d94d4b1dee2664ea26d3130f386cad
RidloGhifary <ridloghfry@gmail.com>
2024-06-06 12:30:16 +0700

    feat: add animation on background header

--- src/components/BackgroundPattern.tsx
+++ src/components/BackgroundPattern.tsx
@@ -1,7 +1,7 @@
 const BackgroundPattern = () => {
   return (
     <>
-      <div className="pattern-boxes pattern-white pattern-bg-transparent pattern-opacity-5 pattern-size-8 absolute bottom-0 left-0 right-0 top-0 h-auto w-full"></div>
+      <div className="pattern-boxes absolute bottom-0 left-0 right-0 top-0 h-auto w-full animate-bounce pattern-bg-transparent pattern-white pattern-opacity-5 pattern-size-8"></div>
       <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-AlmostBlack to-AlmostBlack/0" />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,20 @@ export default {
           "0%": { backgroundPosition: "0" },
           "50%, 100%": { backgroundPosition: "600px" },
         },
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-.8%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
       },
       animation: {
         shine: "shine 3s linear infinite",
+        animation: "bounce 20s infinite",
       },
     },
     patterns: {
